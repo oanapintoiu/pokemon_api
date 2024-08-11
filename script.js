0b92db06e0892cf8d912e55bf3d8d796f3b97b85
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-08-11 10:40:23 +0100

    added new test to ckeck if data if fetched using ID. all tests pass. added basic css styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ async function getPokemonData(pokemonName) {
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
   const response = await fetch(apiUrl);
+  if (!response.ok) throw new Error("Pokemon not found");
 
   return response.json();
 }
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,5 +1,6 @@
-const { default: expect } = require("expect");
+
 const { getPokemonData } = require("../script");
+
 
 describe("getPokemonData", () => {
   it("fetches data from API for a given Pokemon name", async () => {
@@ -7,9 +8,15 @@ describe("getPokemonData", () => {
     expect(data.name).toBe("pikachu");
   });
 
+  it("fetches data from API for a given Pokemon ID", async () => {
+    const data = await getPokemonData(25);
+    expect(data.name).toBe("pikachu")
+  })
+
   it("throws an error if the Pokemon is not found", async () => {
     await expect(getPokemonData("invalidPokemon")).rejects.toThrow(
       "Pokemon not found"
-    );
+    );  
   });
+
 });
